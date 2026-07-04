203c214b0a520d396544590586d9c646dce84b7d
Vedant Bhor <vedant23bhor@gmail.com>
2026-07-04 10:03:29 +0530

    first function created

--- merg_sort_ui.py
+++ merg_sort_ui.py
@@ -3,3 +3,31 @@ import time
 import random
 import tkinter as tk
 from tkinter import messagebox, scrolledtext
+
+
+
+# ------------------- MERGE FUNCTION -------------------
+def merge(arr, left, mid, right):
+    L = arr[left:mid + 1]
+    R = arr[mid + 1:right + 1]
+    i = j = 0
+    k = left
+
+    while i < len(L) and j < len(R):
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+
+    while i < len(L):
+        arr[k] = L[i]
+        i += 1
+        k += 1
+
+    while j < len(R):
+        arr[k] = R[j]
+        j += 1
+        k += 1
