c8ff2b78a67b044707f2ef16c4b7855019feb048
Vedant Bhor <vedant23bhor@gmail.com>
2026-07-04 10:04:19 +0530

    normal merg-sort

--- merg_sort_ui.py
+++ merg_sort_ui.py
@@ -31,3 +31,11 @@ def merge(arr, left, mid, right):
         arr[k] = R[j]
         j += 1
         k += 1
+
+# ------------------- NORMAL MERGE SORT -------------------
+def merge_sort(arr, left, right):
+    if left < right:
+        mid = (left + right) // 2
+        merge_sort(arr, left, mid)
+        merge_sort(arr, mid + 1, right)
+        merge(arr, left, mid, right)
